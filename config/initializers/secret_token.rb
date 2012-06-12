51a7be0330e5273f8415e301d1533a8025b33771
Grant David Bachman <grantbachman@gmail.com>
2012-06-11 20:03:49 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordsmith::Application.config.secret_token = '6b975962350ccb21c84c7a7d3925e11352898c03c2b22d0ce930027e31c6cdd17fe504cebff43270df095be05665096a12089b9840ca9243964b1de61e65a316'
