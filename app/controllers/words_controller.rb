25c9eda65cf86209c194d0d44c1c831d76838c85
Grant David Bachman <grantbachman@gmail.com>
2012-07-12 19:13:27 -0500

    Check Wordnik API for existence of a word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -9,11 +9,18 @@ class WordsController < ApplicationController
 	def create
 		@words = current_user.words.all
 		@word = current_user.words.build(params[:word])
-		if @word.save
-			flash[:success] = "Word added"
-			redirect_to root_path
-		else
+		@theWord = Wordnik.word.get_definitions(params[:word][:name])
+		if @theWord.blank?
+			flash[:error] = "That isn't a real word to our knowledge..."
 			render 'new'
+		else
+			if @word.save
+				flash[:success] = "Word added"
+				redirect_to root_path
+			else
+				flash[:error] = @word.errors.full_messages[0]
+				render 'new'
+			end
 		end
 	end
 end
