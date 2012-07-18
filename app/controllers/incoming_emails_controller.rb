b14d4aa14ca4760a6e73ce5cd49a8af9b1a9f06b
Grant David Bachman <grantbachman@gmail.com>
2012-07-18 17:44:17 -0500

    Incoming parameters

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -3,9 +3,17 @@ class IncomingEmailsController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 
 	def create
-		sender = params['from']
-		subject = params['subject']	
-		actual_body = params["stripped-text"]
+		if (params['from'].include?("grantbachman@gmail.com"))
+			user = User.find_by_email("grantbachman@gmail.com")
+			word = user.words.build(name: "success")
+			word.save	
+		else
+			render text: "This is bad."
+		end
+	end
+
+	def index
+		render text: "This is text. I have spoken."
 	end
 
 end
--- spec/controllers/incoming_emails_controller_spec.rb
+++ spec/controllers/incoming_emails_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe IncomingEmailsController do
 
+	describe "POST 'create'" do
+		it "should have a body that says something" do
+			post :create, "body-plain" => "Grant Bachman" 
+			response.should redirect_to root_url 
+			#response.should have_selector("Grant")
+		end
+	end
 end
