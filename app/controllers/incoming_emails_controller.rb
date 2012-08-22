33ee8369d71fd2c36a188c2c9376e7c9dc8f6946
Grant David Bachman <grantbachman@gmail.com>
2012-08-21 21:33:09 -0400

    Save received answers

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -6,27 +6,59 @@ class IncomingEmailsController < ApplicationController
 
 		body = params(['stripped-text'])
 
-		#find the auth_hash
+		# find the auth_hash
 		if (match = body[/auth_hash:\h{64}:/])
 			auth_hash = match[-65..-2]
 		end
 		quiz = Quiz.find_by_auth_hash(auth_hash)
 
-		#find number of questions in the quiz
-		num_questions = quiz.answer_key.split(',').count
+		# get array of questions
+		questions = Question.where(quiz_id: quiz.id)
+		num_questions = questions.count 
 
+		# get the answer_key_array into the form [['a', 'palindrome'], ['b', 'paradox'], ['c', 'psycho']] 
+		# => If I let the user answer with the word a two-dimensional array will be easier to navigate than a hash
+		answer_key_array = quiz.answer_key.split(',')
+		answer_key_array.each_with_index do |x, index|
+			answer_key_array[index] = x.split(':')
+		end
+
+		# answers array with be in the form of ['a','b','c','d']
+		received_answers_array = parse_email(body, num_questions)
+		# get the received_answers_array into the form [['a', 'palindrome'], ['b', 'paradox'], ['c', 'psycho']] 
+		# => for the same reason as above
+		received_answers_array.each_with_index do |x, index|
+			answer_key_array.each { |y| received_answers_array[index] = y if y[0] == x } 
+		end
+
+		# Save results
+		answer_key_array.each_with_index do |arr, index|
+			question = questions.where(word_id: Word.find_by_name(arr[1]).id).first
+			question.answer = received_answers_array[index][1]
+			if received_answers_array[index][1] == arr[1]
+				question.correct = true
+			else
+				question.correct = false
+			end
+			question.save
+		end
+
+		render nothing: true	
+	end
+
+	def index
+		render text: "This is text. I have spoken."
+	end
+
+	def parse_email(body, num_questions)
 		#find the answer text in the form of [digit][period][letter][comma], and format the string
 		if (match = body[/(\d\s?\.?\s?[a-zA-Z]\s?,\s?){#{num_questions-1}}(\d\s?\.?\s?[a-zA-Z]\s?){1}/])
 			match.gsub!('.','')
 			match.gsub!(' ','')
 			match.gsub!('/\d/','')
 			returned_answers = match.split(',')	
+			return returned_answers
 		end
-			
-	end
-
-	def index
-		render text: "This is text. I have spoken."
-	end
+	end		
 
 end
